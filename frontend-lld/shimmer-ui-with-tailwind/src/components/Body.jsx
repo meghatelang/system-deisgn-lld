9b94c5e9f0873403eec4190401ee052cb6fbaeeb
meghatelang <megha.telang3@gmail.com>
2026-02-03 12:27:28 +0530

    Added Shimmer UI with tailwind

---
+++ frontend-lld/shimmer-ui-with-tailwind/src/components/Body.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import MemeDashboard from './MemeDashboard';
+import Shimmer from './Shimmer';
+
+const Body = () => {
+  const [memes, setMemes] = useState([]);
+
+  useEffect(() => {
+    fetchMemes();
+  }, []);
+
+  const fetchMemes = async () => {
+    const data = await fetch('https://meme-api.com/gimme/20');
+    const json = await data.json();
+    setMemes(json.memes);
+  };
+
+  return (
+    <div className="flex flex-wrap">
+      {memes.length ? <MemeDashboard data={memes} /> : <Shimmer />}
+    </div>
+  );
+};
+
+export default Body;
---
+++ frontend-lld/shimmer-ui-with-tailwind/src/components/MemeDashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MemeDashboard = (props) => {
+  const { data = [] } = props;
+
+  console.log('comes here', data);
+
+  return data.map((meme, index) => (
+    <div className="p-5 m-5 border border-black rounded-lg">
+      <img className="w-64 h-64" alt="meme" src={meme?.url} />
+      <p>{meme?.author}</p>
+    </div>
+  ));
+};
+
+export default MemeDashboard;
