1ed945da10a41b3d979bda5744d414d44eaf209c
Megha Telang <megha.telang3@gmail.com>
2026-05-03 17:27:10 +0530

    Added accordion

--- frontend-lld/src/components/accordion/index.jsx
+++ frontend-lld/src/components/accordion/index.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { ACCORDION_DATA } from "../utils/constants";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 
 const Accordion = () => {
+  const [accordion, setAccordion] = useState(0);
+
   return (
-    <div>
+    <div className="w-100 flex gap-2 flex-col">
       {ACCORDION_DATA.map((acc, i) => (
-        <div key={i}>
-          <div>{acc.title}</div>
-          <FontAwesomeIcon icon={faArrowDown} size="lg" />
-        </div>
+        <>
+          <div
+            key={i}
+            className="flex justify-between border border-gray-300 shadow-lg p-2 bg-sky-200"
+          >
+            <div>{acc.title}</div>
+            <FontAwesomeIcon
+              icon={i === accordion ? faAngleDown : faAngleUp}
+              size="lg"
+              className="cursor-pointer"
+              onClick={() => {
+                i === accordion ? setAccordion(null) : setAccordion(i);
+              }}
+            />
+          </div>
+          {accordion === i && (
+            <div className="border-b border-l border-r border-gray-300 p-2">
+              {acc.body}
+            </div>
+          )}
+        </>
       ))}
     </div>
   );
