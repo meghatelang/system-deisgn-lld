008b6805b2d31316bd9a00f6a698a7fde37c04b0
Megha Telang <megha.telang3@gmail.com>
2026-05-17 13:46:16 +0530

    Updated search css

--- frontend-lld/src/components/autocomplete-search/index.tsx
+++ frontend-lld/src/components/autocomplete-search/index.tsx
@@ -1,5 +1,20 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const AutocompleteSearch = () => {
-  return <div>Hi</div>;
+  return (
+    <div className="relative w-100 p-2 border border-gray-400 rounded-full">
+      <input
+        type="text"
+        placeholder="Enter the search text"
+        className="w-full outline-none"
+      />
+      <FontAwesomeIcon
+        icon={faSearch}
+        className="absolute right-3 top-1/2 -translate-y-1/2"
+      />
+    </div>
+  );
 };
 
 export default AutocompleteSearch;
