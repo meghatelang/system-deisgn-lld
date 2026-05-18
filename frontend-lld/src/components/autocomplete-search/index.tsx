5fc1c45e3650bf99ffa46237d809f2b9649a1a70
Megha Telang <megha.telang3@gmail.com>
2026-05-18 21:13:13 +0530

    Added Autocomplete search changes

--- frontend-lld/src/components/autocomplete-search/index.tsx
+++ frontend-lld/src/components/autocomplete-search/index.tsx
@@ -1,19 +1,55 @@
+import React, { useState, useEffect } from "react";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const AutocompleteSearch = () => {
+  const [searchRes, setSearchRes] = useState<Array<string>>([]);
+  const [searchText, setSearchText] = useState<string>("");
+  const [showResults, setShowResults] = useState<Boolean>(false);
+
+  useEffect(() => {
+    fetchData();
+  }, [searchText]);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://www.google.com/complete/search?client=firefox&q=" + searchText,
+    );
+
+    const data = await response.json();
+
+    setSearchRes(data[1]);
+  };
+
   return (
-    <div className="relative w-100 p-2 border border-gray-400 rounded-full">
-      <input
-        type="text"
-        placeholder="Enter the search text"
-        className="w-full outline-none"
-      />
-      <FontAwesomeIcon
-        icon={faSearch}
-        className="absolute right-3 top-1/2 -translate-y-1/2"
-      />
-    </div>
+    <>
+      <div className="relative w-100 p-2 border border-gray-400">
+        <input
+          type="text"
+          placeholder="Enter the search text"
+          className="w-full outline-none"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          onFocus={() => {
+            setShowResults(true);
+          }}
+          onBlur={() => {
+            setShowResults(false);
+          }}
+        />
+        <FontAwesomeIcon
+          icon={faSearch}
+          className="absolute right-3 top-1/2 -translate-y-1/2"
+        />
+      </div>
+      {searchRes.length > 1 && showResults && (
+        <ul className="border-l w-100 border-r border-b border-gray-400">
+          {searchRes?.map((str, i) => (
+            <li key={i}>{str}</li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
